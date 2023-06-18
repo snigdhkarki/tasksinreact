bd43b56c2376ab3564e316a3d217e23bf6ac36aa
Snigdh Karki <snigdhkarki@gmail.com>
2023-06-18 21:17:07 +0545

    very final

--- src/App.js
+++ src/App.js
@@ -17,12 +17,12 @@ class App extends Component{
   render(){
     return(
       <div>
-      <HelmetProvider>
+      
       <Helmet>
       <title>To do list</title>
       <meta name='description' content='This is a todo list that help make todo list' />
       </Helmet>
-      </HelmetProvider>
+      
       <input type = "text" className="enter"></input>
       <button type = "button" onClick={this.addtask}>SUBmit</button>
       {this.state.tasks.map((task)=>(
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <HelmetProvider>
     <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
 
